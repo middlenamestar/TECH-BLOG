cfc4625d1879d2f6659a3b29d78233c1f4b92f9a
priscilla ma <priscillama621@gmail.com>
2022-03-09 21:37:03 -0500

    wrote get routes for allposts and single post

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,12 +1,35 @@
 const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
 
-// home page.
+// home page. all posts.
 router.get('/', async (req, res) => {
     try {
-        // res.render('home');
-        res.json('all posts♡')
+        const posts = await Post.findAll({
+            include: [User],
+        });
+        const post = posts.map((el) => el.get({ plain: true }));
+        // console.log(post);
+        res.render('home', {post});
     } catch (err) {
-        res.json('uh oh. error');
+        res.status(500).json(err);
+    }
+});
+
+// get route for single post.
+router.get('/post/:id', async (req, res) => {
+    try {
+        id = req.params.id;
+        const posts = await Post.findOne({where: {id},
+            include: [
+                User,
+                {model: Comment, include: [User]}
+            ]
+        });
+        const post = posts.get({plain: true});
+        console.log(post);
+        res.render('individual-post', {post})
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
