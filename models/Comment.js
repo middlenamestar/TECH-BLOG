fe255af5f5c94ecac8fee11a13bc250512161bb3
priscilla ma <priscillama621@gmail.com>
2022-03-09 21:37:36 -0500

    added Models and created relationships.

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments'
+  }
+);
+
+module.exports = Comment;
---
+++ models/Post.js
@@ -0,0 +1,20 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts'
+  }
+);
+
+module.exports = Post;
