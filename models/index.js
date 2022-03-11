66ae8ee762e6e38785ec092767bc3bdeb867878e
priscilla ma <priscillama621@gmail.com>
2022-03-10 21:52:15 -0500

    changed to underscore format for the FKs

--- models/index.js
+++ models/index.js
@@ -3,23 +3,23 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
 
 
 Post.hasMany(Comment, {
-    foreignKey: 'postId',
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
 
