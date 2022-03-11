68f5e96870a3343693e458b2e3ce28236b6bf67c
priscilla ma <priscillama621@gmail.com>
2022-03-10 21:52:33 -0500

    added dash route to index

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeRoute = require('./homeRoute');
+const dashRoute = require('./dashRoute');
 
 router.use('/', homeRoute);
+router.use('/my-dash', dashRoute);
 
 module.exports = router;
