80c4a97161870d1af0635d6d59a8d749dcd7d3cd
priscilla ma <priscillama621@gmail.com>
2022-03-12 17:58:48 -0500

    added userRoutes to index

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeRoute = require('./homeRoute');
 const dashRoute = require('./dashRoute');
+const userRoutes = require('./userRoutes');
 
 router.use('/', homeRoute);
 router.use('/my-dash', dashRoute);
+router.use('/user', userRoutes);
 
 module.exports = router;
